47a9c6e3b154cf16f66c305ac7cd4b24f55fccd3
Muhammed <107894012+Bidz42@users.noreply.github.com>
2022-08-20 15:51:07 +0100

    more work more bugs

--- Project-2/models/Review.model.js
+++ Project-2/models/Review.model.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 const reviewSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
   comment: { type: String, maxlength: 200 },
+  star: { type:String }
 });
 
 const Review = model("Review", reviewSchema);
